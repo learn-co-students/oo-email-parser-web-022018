1a2fceeb081c06a4881a8fc60f6a611bdbd5313c
bafab <bafa@tutanota.de>
2018-02-26 14:53:38 -0500

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,9 +1,11 @@
 class EmailParser
+
   def initialize(emails = nil)
     @emails = emails
   end
 
   def parse
-    (@emails.gsub(","," ").split(" ")).uniq
+    (@emails.split(/[, ]+/)).uniq
   end
+
 end
